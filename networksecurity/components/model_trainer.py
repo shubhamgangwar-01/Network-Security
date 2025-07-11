20492467bf29c71e839c0ef6852615dba148ba12
Shubham Gangwar <gangwarshubham490@gmail.com>
2025-07-11 10:34:23 +0530

    Trainer Updated

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,15 +27,6 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
-
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
-
-
-
-
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
